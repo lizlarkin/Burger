316680e77d79a7167a9a864c8adaa0bb10a6c668
Liz <lizlarkin@highlandtechnology.com>
2021-01-06 20:54:13 -0800

    added database schema and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Western Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("In N Out Burger", FALSE);
