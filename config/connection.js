c64dfb671e541d36c54bbdbbdf4f11389ce38a74
Liz <lizlarkin@highlandtechnology.com>
2021-01-13 14:24:33 -0800

    added JAWDB for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
-const mysql = require('mysql');
+var mysql = require('mysql');
 
 // const PORT = process.env.PORT || 8080;
 
-const connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'password',
     database: 'burgers_db',
   });
+};
   
   connection.connect(function(err) {
     if (err) {
