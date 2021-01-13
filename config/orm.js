81c471a60459596d0c0646f95f29977e53cce870
Liz <lizlarkin@highlandtechnology.com>
2021-01-12 22:20:02 -0800

    added background and basic styling to sections more apparent

--- config/orm.js
+++ config/orm.js
@@ -2,6 +2,29 @@
 // MySQL connection is connected here
 const connection = require('./connection.js');
 
+// Helper function to convert object key/value pairs to SQL syntax
+const objToSql = (ob) => {
+    const arr = [];
+  
+    // Loop through the keys and push the key/value as a string int arr
+    for (const key in ob) {
+      let value = ob[key];
+      // Check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // If string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+          value = `'${value}'`;
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(`${key}=${value}`);
+      }
+    }
+  
+    // Translate array of strings to a single comma-separated string
+    return arr.toString();
+  };
+
 const orm = {
 
     // Select All Method
@@ -24,9 +47,15 @@ const orm = {
 
     // Update One Method
     updateOne (tableName, boolean, condition, cb) {
-        // const queryString = `UPDATE ${tableName} SET devoured = ${boolean} WHERE id = ${condition}`;
-        const queryString = `UPDATE ${tableName} SET devoured = ${boolean} WHERE ${condition}`;
+        // const queryString = `UPDATE ${tableName} SET devoured = ${boolean} WHERE ${condition}`;
+        const queryString = "UPDATE " + tableName;
+        queryString += " SET "
+        queryString += objToSql(boolean) 
+        queryString += " WHERE "
+        queryString += condition;
+
         console.log(queryString);
+
         // connection.query(queryString, boolean, condition, (err, data) => {
         connection.query(queryString, (err, data) => {
             if (err) {throw err};
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -95,3 +95,4 @@ devouredBtns.forEach((btn) => {
 //       })
 //     });
 //   });
+// });
